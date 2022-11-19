eb76cdbb60cf11bc91c9a81b4f9bd9b0452d9e85
nitish <nitishkumarmay30@gmail.com>
2022-11-19 19:21:00 +0530

    Add solution for problem `C - FF` Link: https://atcoder.jp/contests/abc278/tasks/abc278_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc278/submissions/36645520

--- atcoder/abc278/C.py
+++ atcoder/abc278/C.py
@@ -1,14 +1,42 @@
-N, Q = map(int, input().split())
-cnt = set()
- 
-for _ in range(Q):
-  T, A, B = map(int, input().split())
-  if T == 1:
-    cnt.add((A, B))
-  if T == 2:
-    cnt.discard((A, B))
-  if T == 3:
-    if (A, B) in cnt and (B, A) in cnt:
-      print('Yes')
-    else:
-      print('No')
+
+
+N,Q=map(int, input().split())
+'''
+## same approach using dictionary but, alas, it didnt work out
+from collections import defaultdict
+for i in range(N):
+    dic[i+1]=set()
+for i in range(Q):
+    arr =list(map(int, input().split()))
+    if arr[0]==1:
+        dic[arr[1]].add(arr[2])
+    elif arr[0]==2:
+        dic[arr[1]].discard(arr[2])
+    elif arr[0]==3:
+        if arr[1] in dic[arr[2]]:
+            if arr[2] in dic[arr[1]]:
+                print("Yes")
+            else:
+                print("No")
+        else:
+            print("No")
+
+'''
+follows=set()
+
+for i in range(Q):
+    arr=list(map(int, input().split()))
+    if arr[0]==1:
+        follows.add((arr[1],arr[2]))
+    elif arr[0]==2:
+        follows.discard((arr[1],arr[2]))
+    elif arr[0]==3:
+        if ((arr[1],arr[2])) in follows:
+            if ((arr[2],arr[1])) in follows:
+                print("Yes")
+            else:
+                print("No")
+        else:
+            print("No")
+
+
