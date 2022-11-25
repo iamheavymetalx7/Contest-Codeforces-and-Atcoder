919c5cb1f82cc7419caad69d95b61cc267dca115
nitish <nitishkumarmay30@gmail.com>
2022-11-26 00:43:00 +0530

    Add solution for problem `B - XOR = Average` Link: https://codeforces.com/contest/1758/problem/B Tags: constructive algorithms, *900 Ref: https://codeforces.com/contest/1758/submission/182557635

---
+++ codeforces/1758/B.py
@@ -0,0 +1,49 @@
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+def solve(t):
+    n=int(input())
+    if n%2==1:
+        a=[69]*n
+        print(*a)
+    else:
+        a=[2]*(n-2)+[1,3]
+        print(*a)
+ 
+ 
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        solve(i+1)
+ 
+ 
+if __name__ == '__main__':
+    main()
