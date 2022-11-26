9a2ab55045a631f9d4b360ea9543fef5f4d354cf
nitish <nitishkumarmay30@gmail.com>
2022-11-26 15:43:00 +0530

    Add solution for problem `B - Bad Boy` Link: https://codeforces.com/contest/1537/problem/B Tags: constructive algorithms, greedy, math, *900 Ref: https://codeforces.com/contest/1537/submission/182626976

---
+++ codeforces/1537/B.py
@@ -0,0 +1,49 @@
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+def solve(t):
+    n,m,i,j=map(int,input().split())    
+    
+    left_most = [1,1]
+    right_most=[n,m]
+
+    print(*left_most,end=" ")
+    print(*right_most)
+
+ 
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        solve(i+1)
+ 
+ 
+if __name__ == '__main__':
+    main()
