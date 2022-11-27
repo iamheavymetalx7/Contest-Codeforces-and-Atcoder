03d6b8db4c83714a6d66379f14fa14d89de5931a
nitish <nitishkumarmay30@gmail.com>
2022-11-27 09:06:00 +0530

    Add solution for problem `C - Alice and Bob` Link: https://codeforces.com/contest/347/problem/C Tags: games, math, number theory, *1600 Ref: https://codeforces.com/contest/347/submission/182736622

---
+++ codeforces/347/C.py
@@ -0,0 +1,56 @@
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+def solve():
+    n=int(input())
+    a=list(map(int,input().split()))
+    g=0
+    a.sort()
+
+    for i in range(n):
+        g=math.gcd(g,a[i])
+    
+    max_n=a[-1]//g
+
+    numbers_added = max_n-n
+
+    if numbers_added%2:
+        print("Alice")
+    else:
+        print("Bob")
+
+ 
+ 
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+
+    solve()
+ 
+ 
+if __name__ == '__main__':
+    main()
