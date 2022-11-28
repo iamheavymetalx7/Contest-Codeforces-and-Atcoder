fb597febda9a1fa1e369c8748d49390eedc60891
nitish <nitishkumarmay30@gmail.com>
2022-11-28 16:17:00 +0530

    Add solution for problem `A - Mainak and Array` Link: https://codeforces.com/contest/1726/problem/A Tags: greedy, math, *900 Ref: https://codeforces.com/contest/1726/submission/183001210

---
+++ codeforces/1726/A.py
@@ -0,0 +1,60 @@
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+def solve(t):
+    n=ii()
+    a=lmii()    
+    ans=0
+    for i in range(n):
+        ans=max(ans,a[i]-a[0])      ##keepin 1st elt fixed, rotating others as last
+        ans=max(ans,a[-1]-a[i])     ## keeping the last elt fixed, and rotating others
+
+        ans=max(ans,a[i]-a[(i+1)%n])    ##keeping the ith elt as last elt, then i+1 is at the first position
+        ##keeping the ith elt at first position is also covered in the above case as well
+
+    print(ans)
+
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        solve(i+1)
+ 
+ 
+if __name__ == '__main__':
+    main()
