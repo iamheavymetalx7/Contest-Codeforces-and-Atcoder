b9dc2434dcbe6f962e144f119ef438608268ebb4
nitish <nitishkumarmay30@gmail.com>
2022-11-29 14:55:00 +0530

    Add solution for problem `C - Zero-Sum Prefixes` Link: https://codeforces.com/contest/1748/problem/C Tags: brute force, data structures, dp, greedy, implementation, *1600 Ref: https://codeforces.com/contest/1748/submission/183168869

---
+++ codeforces/1748/C.py
@@ -0,0 +1,76 @@
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+def solve(t):
+    n=ii()
+    a=lmii()
+
+    prefix=[0]*n
+
+    prefix[0]=a[0]
+
+    for i in range(1,n):
+        prefix[i] = prefix[i-1]+a[i]
+    
+    ans=0
+    dic={}
+    i=n-1
+    while i>=0:
+        dic[prefix[i]]=dic.get(prefix[i],0)+1
+
+        if a[i]==0:
+            ans+=max(list(dic.values()))
+            dic={}
+        i-=1
+
+    ans+=dic.get(0,0)
+    return ans
+
+
+
+
+    
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        z = solve(i+1)
+        print(z)
+ 
+if __name__ == '__main__':
+    main()
