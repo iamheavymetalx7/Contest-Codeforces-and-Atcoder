a8063f08a02b80a8be4d8c68a79e096493e5b687
nitish <nitishkumarmay30@gmail.com>
2022-12-01 19:53:00 +0530

    Add solution for problem `C - Minimize the Thickness` Link: https://codeforces.com/contest/1741/problem/C Tags: brute force, greedy, math, two pointers, *1100 Ref: https://codeforces.com/contest/1741/submission/183489825

---
+++ codeforces/1741/C.py
@@ -0,0 +1,71 @@
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+def solve(t):
+    n=ii()
+    l=lmii()
+    prefix=[0]*n
+    prefix[0]=l[0]
+    for i in range(0,n-1):
+        prefix[i+1] =prefix[i]+l[i+1]
+    
+
+    ans=n
+
+    for i in range(n):
+        res=i+1
+        k=i
+        for j in range(i+1,n):
+            if prefix[j]==prefix[i]+prefix[k]:
+                res=max(res,j-k)
+                k=j
+        if k==n-1:
+            ans=min(ans,res)
+    print(ans)
+
+
+
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        solve(i+1)
+ 
+ 
+if __name__ == '__main__':
+    main()
