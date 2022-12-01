29885dee6a1d6f08f13949de0849fc0ada2d8338
nitish <nitishkumarmay30@gmail.com>
2022-12-01 16:22:00 +0530

    Add solution for problem `C - Unstable String` Link: https://codeforces.com/contest/1535/problem/C Tags: binary search, dp, greedy, implementation, strings, two pointers, *1400 Ref: https://codeforces.com/contest/1535/submission/183464998

---
+++ codeforces/1535/C.py
@@ -0,0 +1,64 @@
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+def solve(t):
+    s=si()
+
+    x=[-1,-1]
+    ans=0
+
+    for i in range(len(s)):
+        c=ord(s[i])-ord('0')
+        if c==0 or c==1:
+            x[c^(i%2)]=i
+        
+        mn=min(x[0],x[1])
+
+        ans+=i-mn
+    print(ans)
+    
+    
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        solve(i+1)
+ 
+ 
+if __name__ == '__main__':
+    main()
