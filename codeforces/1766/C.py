41965defb08e60d5ab560833cbb069de184b6573
nitish <nitishkumarmay30@gmail.com>
2022-12-15 18:22:00 +0530

    Add solution for problem `C - Hamiltonian Wall` Link: https://codeforces.com/contest/1766/problem/C Tags: dp, greedy, implementation Ref: https://codeforces.com/contest/1766/submission/185287739

---
+++ codeforces/1766/C.py
@@ -0,0 +1,66 @@
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+def solve(t):
+    n=ii()
+    s1=si()
+    s2=si()
+
+    k1,k2=1,1
+
+    for i in range(n):
+        if s1[i]=="B" and s2[i]=="B":
+            k1,k2=k2,k1
+        elif s1[i]=="B":
+            k2=0
+        else:k1=0
+
+    if k1 or k2:
+        print("YES")
+    else:
+        print("NO")    
+    
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        solve(i+1)
+ 
+ 
+if __name__ == '__main__':
+    main()
