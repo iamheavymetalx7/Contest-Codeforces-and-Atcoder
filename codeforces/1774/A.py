ebc1367b92b8ab370e3f30159ab9bf5c2aa142b3
nitish <nitishkumarmay30@gmail.com>
2022-12-18 01:37:00 +0530

    Add solution for problem `A - Add Plus Minus Sign` Link: https://codeforces.com/contest/1774/problem/A Tags: constructive algorithms Ref: https://codeforces.com/contest/1774/submission/185726104

---
+++ codeforces/1774/A.py
@@ -0,0 +1,66 @@
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+def solve(t):
+    n=ii()
+    s=si()
+    flag=1
+    string=""
+    for i in range(n):
+        if s[i]=="1" and flag%2:
+            string+="+"
+            flag+=1
+        elif s[i]=="1" and not(flag%2):
+            string+="-"
+            flag+=1
+        else:
+            string+="+"
+    print(string[1:])
+
+
+    
+    
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        solve(i+1)
+ 
+ 
+if __name__ == '__main__':
+    main()
