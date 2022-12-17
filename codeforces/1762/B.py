d088e21ab03c35908d49e77c196dad4922cf9d2b
nitish <nitishkumarmay30@gmail.com>
2022-12-17 20:23:00 +0530

    Add solution for problem `B - Make Array Good` Link: https://codeforces.com/contest/1762/problem/B Tags: constructive algorithms, implementation, number theory, sortings Ref: https://codeforces.com/contest/1762/submission/185651476

---
+++ codeforces/1762/B.py
@@ -0,0 +1,69 @@
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+def solve(t):
+    n=ii()
+    nums=lmii()
+
+    arr=[]
+    ans=[]
+    for i in range(n):
+        arr.append([nums[i],i])
+    
+    arr.sort()
+
+
+    for i in range(1,n):
+        if arr[i][0]%arr[i-1][0]!=0:
+            x = (int(arr[i][0]//arr[i-1][0])+1)*(arr[i-1][0]) - arr[i][0]
+            ans.append([arr[i][1]+1,x])
+            arr[i][0]+=x
+    
+    print(len(ans))
+    for i in range(len(ans)):
+        print(*ans[i])
+    
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        solve(i+1)
+ 
+ 
+if __name__ == '__main__':
+    main()
