aae4bcaad1c07436088bcaf86f76dfa7339322da
nitish <nitishkumarmay30@gmail.com>
2022-12-17 19:52:00 +0530

    Add solution for problem `A - Divide and Conquer` Link: https://codeforces.com/contest/1762/problem/A Tags: greedy, math, number theory Ref: https://codeforces.com/contest/1762/submission/185634190

---
+++ codeforces/1762/A.py
@@ -0,0 +1,68 @@
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+def solve(t):
+    n=ii()
+    arr=lmii()
+
+    ans=pow(10,6)
+
+    if sum(arr)%2==0:
+        return 0
+
+    for i in range(n):
+        j=arr[i]
+        ops=0
+        while arr[i]%2 == j%2:
+            ops+=1
+            j=j//2
+        ans=min(ops,ans)
+    return ans
+            
+    
+
+    
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        z= solve(i+1)
+        print(z)
+ 
+if __name__ == '__main__':
+    main()
