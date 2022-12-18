ab0ac14492f4212af2e406a8669248a429bd1202
nitish <nitishkumarmay30@gmail.com>
2022-12-18 20:45:00 +0530

    Add solution for problem `C - Different Differences` Link: https://codeforces.com/contest/1772/problem/C Tags: greedy, math Ref: https://codeforces.com/contest/1772/submission/185836893

---
+++ codeforces/1772/C.py
@@ -0,0 +1,78 @@
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+def solve(t):
+    k,n=mii()
+
+    arr=[False]*n
+    j=1
+    a=1
+    to_ret=[]
+    to_ret.append(j)
+    arr[j-1]=True
+    length=1
+    for i in range(n):
+        while length<k and j<=n:
+            j+=a
+            a+=1
+            if j<=n:
+                length+=1
+                to_ret.append(j)
+                arr[j-1]=True
+    
+    length = len(to_ret)
+
+    i=n-1
+    while length<k and i>0:
+            if arr[i]==False:
+                to_ret.append(i+1)
+                length+=1
+                arr[i]=True
+            i-=1
+    print(*sorted(to_ret))
+
+    
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        solve(i+1)
+ 
+ 
+if __name__ == '__main__':
+    main()
