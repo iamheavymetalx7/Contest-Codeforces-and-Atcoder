84e0761ed4750cab3eb849e76ebd8acfe9b37ce1
nitish <nitishkumarmay30@gmail.com>
2022-12-18 14:59:00 +0530

    Add solution for problem `B - Basketball Together` Link: https://codeforces.com/contest/1725/problem/B Tags: binary search, greedy, sortings, *1000 Ref: https://codeforces.com/contest/1725/submission/185770363

---
+++ codeforces/1725/B.py
@@ -0,0 +1,64 @@
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+def solve():
+    N,D = mii()
+    A =lmii()
+    A.sort()
+    
+    c=0
+    a=0
+
+    while A and a<=N:
+        a+= D//A[-1]+1
+        A.pop(-1)
+
+        if a<=N:
+            c+=1
+    print(c)
+
+       
+    
+    
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+ 
+    solve()
+ 
+ 
+if __name__ == '__main__':
+    main()
