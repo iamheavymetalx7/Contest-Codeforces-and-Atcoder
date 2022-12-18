e272e9353fe88b12e3a7e2eae0c0200692fce6e8
nitish <nitishkumarmay30@gmail.com>
2022-12-18 19:50:00 +0530

    Add solution for problem `C - Min-Max Array Transformation` Link: https://codeforces.com/contest/1721/problem/C Tags: binary search, greedy, two pointers, *1400 Ref: https://codeforces.com/contest/1721/submission/185796852

--- README.md
+++ README.md
@@ -13,8 +13,9 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-79 | [C - Hossam and Trainees](https://codeforces.com/contest/1771/problem/C) | [PyPy 3-64](./codeforces/1771/C.py) | `greedy` `math` `number theory` `*1600` | Dec/19/2022 11:00 | 
-78 | [A - A+B?](https://codeforces.com/contest/1772/problem/A) | [PyPy 3-64](./codeforces/1772/A.py) | `implementation` | Dec/18/2022 20:06 | 
+80 | [C - Hossam and Trainees](https://codeforces.com/contest/1771/problem/C) | [PyPy 3-64](./codeforces/1771/C.py) | `greedy` `math` `number theory` `*1600` | Dec/19/2022 11:00 | 
+79 | [A - A+B?](https://codeforces.com/contest/1772/problem/A) | [PyPy 3-64](./codeforces/1772/A.py) | `implementation` | Dec/18/2022 20:06 | 
+78 | [C - Min-Max Array Transformation](https://codeforces.com/contest/1721/problem/C) | [PyPy 3-64](./codeforces/1721/C.py) | `binary search` `greedy` `two pointers` `*1400` | Dec/18/2022 19:50 | 
 77 | [B - Coloring](https://codeforces.com/contest/1774/problem/B) | [PyPy 3-64](./codeforces/1774/B.py) | `constructive algorithms` `greedy` | Dec/18/2022 02:06 | 
 76 | [A - Add Plus Minus Sign](https://codeforces.com/contest/1774/problem/A) | [PyPy 3-64](./codeforces/1774/A.py) | `constructive algorithms` | Dec/18/2022 01:37 | 
 75 | [B - Make Array Good](https://codeforces.com/contest/1762/problem/B) | [PyPy 3-64](./codeforces/1762/B.py) | `constructive algorithms` `implementation` `number theory` `sortings` | Dec/17/2022 20:23 | 
---
+++ codeforces/1721/C.py
@@ -0,0 +1,75 @@
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+def solve(t):
+    n=ii()
+    a=lmii()
+    b=lmii()
+
+    j=0
+    for i in range(n):
+        while b[j]<a[i]:
+            j+=1
+        print(b[j]-a[i],end=" ")
+    print()
+        
+    j=n-1
+
+    d_max=[0]*n
+    d_max[n-1] = b[n-1]-a[n-1]
+
+    for i in range(n-2,-1,-1):
+        if a[i+1]<=b[i]:
+            d_max[i]=b[j]-a[i]
+        else:
+            j=i
+            d_max[i]=b[i]-a[i]
+
+    print(*d_max)
+
+
+    
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        solve(i+1)
+ 
+ 
+if __name__ == '__main__':
+    main()
