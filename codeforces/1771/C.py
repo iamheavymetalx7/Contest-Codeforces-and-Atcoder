b54e87a0ba62c6685a862ffa44e8e869c174cb03
nitish <nitishkumarmay30@gmail.com>
2022-12-19 11:00:00 +0530

    Add solution for problem `C - Hossam and Trainees` Link: https://codeforces.com/contest/1771/problem/C Tags: greedy, math, number theory, *1600 Ref: https://codeforces.com/contest/1771/submission/185911045

--- codeforces/1771/C.py
+++ codeforces/1771/C.py
@@ -1,18 +1,50 @@
-from collections import Counter
+#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
+#from collections import deque as dq #deque  e.g. myqueue=dq(list)
+#append/appendleft/appendright/pop/popleft
+#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
+#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
+#import statistics as stat  # stat.median(a), mode, mean
+#from itertools import permutations(p,r)#combinations(p,r)
+#combinations(p,r) gives r-length tuples #combinations_with_replacement
+#every element can be repeated
+        
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
 from math import gcd
-def linar_seive(n):
-    is_composite=[False for i in range(n+1)]
-    primes=[]
-    for i in range(2,n+1):
-        if not is_composite[i]:
-            primes+=[i]
-        j=0
-        while j<len(primes) and i*primes[j]<=n:
-            is_composite[i*primes[j]]=True
-            if i%primes[j]==0:
-                break
-            j+=1
-    return primes
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+
+
+def memodict(f):
+    """memoization decorator for a function taking a single argument"""
+    class memodict(dict):
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret
+
+    return memodict().__getitem__
+
 
 def pollard_rho(n):
     """returns a random factor of n"""
@@ -20,9 +52,10 @@ def pollard_rho(n):
         return 2
     if n % 3 == 0:
         return 3
- 
+
     s = ((n - 1) & (1 - n)).bit_length() - 1
     d = n >> s
+    # for a in [2, 3, 5, 7]:
     for a in [2, 325, 9375, 28178, 450775, 9780504, 1795265022]:
         p = pow(a, d, n)
         if p == 1 or p == n - 1 or a % n == 0:
@@ -45,30 +78,55 @@ def pollard_rho(n):
                 if f != n:
                     return f
     return n
- 
- 
 
+
+@memodict
 def prime_factors(n):
     """returns a Counter of the prime factorization of n"""
     if n <= 1:
         return Counter()
     f = pollard_rho(n)
     return Counter([n]) if f == n else prime_factors(f) + prime_factors(n // f)
-t=int(input())
 
-def solve():
-    n=int(input())
-    arr=list(map(int,input().split()))
-    check=set()
-    for number in arr:
-        for num in prime_factors(number):
-            if num in check:
-                print('YES')
-                return
-            check.add(num)
-        
-    print('NO')
 
-while t:
-    solve()
-    t-=1
+    
+def solve(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    L = []
+    for i in range(n-1):
+        if arr[i] == arr[i+1] and arr[i] != 1:
+            return "YES"
+    s = set()
+    for a in arr:
+        if a != 1:
+            for p in prime_factors(a):
+                if p in s:
+                    return "YES"
+                s.add(p)
+    return "NO"
+    
+    
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        z = solve(i+1)
+        print(z)
+ 
+ 
+if __name__ == '__main__':
+    main()
+    
+ 
