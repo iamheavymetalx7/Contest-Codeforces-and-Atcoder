5a09d645a28dee3ba7b618f918114cc6d53076de
nitish <nitishkumarmay30@gmail.com>
2022-12-19 10:57:00 +0530

    Add solution for problem `C - Hossam and Trainees` Link: https://codeforces.com/contest/1771/problem/C Tags: greedy, math, number theory, *1600 Ref: https://codeforces.com/contest/1771/submission/185910917

--- codeforces/1771/C.py
+++ codeforces/1771/C.py
@@ -1,39 +1,5 @@
-#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
-#from collections import deque as dq #deque  e.g. myqueue=dq(list)
-#append/appendleft/appendright/pop/popleft
-#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
-#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
-#import statistics as stat  # stat.median(a), mode, mean
-#from itertools import permutations(p,r)#combinations(p,r)
-#combinations(p,r) gives r-length tuples #combinations_with_replacement
-#every element can be repeated
-        
-import sys, threading
-import math
-import time
-from os import path
-from collections import defaultdict, Counter, deque
-from bisect import *
-from string import ascii_lowercase
-from functools import cmp_to_key
-import heapq
- 
+from collections import Counter
 from math import gcd
-# # # # # # # # # # # # # # # #
-#       JAI SHREE RAM         #
-# # # # # # # # # # # # # # # #
- 
- 
-def lcm(a, b):
-    return (a*b)//(math.gcd(a,b))
- 
- 
-si= lambda:str(input())
-ii = lambda: int(input())
-mii = lambda: map(int, input().split())
-lmii = lambda: list(map(int, input().split()))
-i2c = lambda n: chr(ord('a') + n)
-c2i = lambda c: ord(c) - ord('a')
 
 
 def memodict(f):
@@ -89,8 +55,7 @@ def prime_factors(n):
     return Counter([n]) if f == n else prime_factors(f) + prime_factors(n // f)
 
 
-    
-def solve(t):
+def solve():
     n = int(input())
     arr = list(map(int, input().split()))
     arr.sort()
@@ -106,27 +71,11 @@ def solve(t):
                     return "YES"
                 s.add(p)
     return "NO"
-    
-    
-def main():
-    t = 1
-    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
-        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
-        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
-        start_time = time.time()
-        print("--- %s seconds ---" % (time.time() - start_time))
- 
- 
-    sys.setrecursionlimit(10**5)
- 
-    t = int(input())
- 
-    for i in range(t):
-        z = solve(i+1)
-        print(z)
- 
- 
-if __name__ == '__main__':
-    main()
-    
- 
+
+
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+t = int(input())
+for i in range(t):
+    print(solve())
