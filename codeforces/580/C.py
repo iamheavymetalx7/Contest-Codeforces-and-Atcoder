6406e607e48febc67436b42e9e6152245d137038
nitish <nitishkumarmay30@gmail.com>
2022-12-22 18:28:00 +0530

    Add solution for problem `C - Kefa and Park` Link: https://codeforces.com/contest/580/problem/C Tags: dfs and similar, graphs, trees, *1500 Ref: https://codeforces.com/contest/580/submission/186354352

--- codeforces/580/C.py
+++ codeforces/580/C.py
@@ -1,61 +1,105 @@
+#bisect.bisect_left(a, x, lo=0, hi=len(a)) is the analog of std::lower_bound()
+#bisect.bisect_right(a, x, lo=0, hi=len(a)) is the analog of std::upper_bound()
+#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
+#from collections import deque as dq #deque  e.g. myqueue=dq(list)
+#append/appendleft/appendright/pop/popleft
+#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
+#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
+#import statistics as stat  # stat.median(a), mode, mean
+#from itertools import permutations(p,r)#combinations(p,r)
+#combinations(p,r) gives r-length tuples #combinations_with_replacement
+#every element can be repeated
+        
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
 si= lambda:str(input())
 ii = lambda: int(input())
 mii = lambda: map(int, input().split())
 lmii = lambda: list(map(int, input().split()))
 i2c = lambda n: chr(ord('a') + n)
 c2i = lambda c: ord(c) - ord('a')
-# PYRIVAL BOOTSTRAP
-# https://github.com/cheran-senthil/PyRival/blob/master/pyrival/misc/bootstrap.py
-from types import GeneratorType
-from collections import defaultdict
-def bootstrap(f, stack=[]):
-    def wrappedfunc(*args, **kwargs):
-        if stack:
-            return f(*args, **kwargs)
-        else:
-            to = f(*args, **kwargs)
-            while True:
-                if type(to) is GeneratorType:
-                    stack.append(to)
-                    to = next(to)
-                else:
-                    stack.pop()
-                    if not stack:
-                        break
-                    to = stack[-1].send(to)
-            return to
 
-    return wrappedfunc
+def BFS(adj,n,cats,m):
+    q=deque()
+    q.append(1)
+    global res
+    vis=[False]*(n+1)
 
-@bootstrap
-def dfs(u,p,numC,maxC,a,graph,m):
-    global ans
-    if a[u]:
-        numC+=1
-    else:
-        numC=0
+
+    while q:
+        curr=q.popleft()
+        vis[curr]=True
+
+        if cats[curr]>m:
+            continue
+            
+        child=0
+        for i in adj[curr]:
+            if not vis[i]:
+                vis[i]=True
+                q.append(i)
+
+                if cats[i]>0:
+                    cats[i]+=cats[curr]
+                child+=1
+        
+        if child ==0:
+            res+=1
+
+
     
-    maxC=max(maxC,numC)
-
-    numChild=0
-
-    for v in graph[u]:
-        if v!=p:
-            yield dfs(v,u,numC,maxC,a,graph,m)
-            numChild+=1
     
-    if numChild==0 and maxC<=m:
-        ans+=1
-    yield ans
+def solve():
+    n,m=mii()
+    adj=defaultdict(list)
+    cats =[0]+lmii()
+    for i in range(n-1):
+        u,v=mii()
+        adj[u].append(v)
+        adj[v].append(u)
+    
+    global res
+    res=0
+    BFS(adj,n,cats,m)
 
-n, m = mii()
-a = [0] + lmii()
-graph = defaultdict(list)
-for i in range(n-1):
-    u,v=mii()
-    graph[u].append(v)
-    graph[v].append(u)
+    print(res)
 
-global ans
-ans=0
-print(dfs(1,-1,0,0,a,graph,m))
+    
+    
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+ 
+    solve()
+ 
+ 
+if __name__ == '__main__':
+    main()
+    
+ 
