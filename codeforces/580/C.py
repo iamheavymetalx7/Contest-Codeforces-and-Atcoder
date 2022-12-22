d52c22544c83b152f8ec60c4b0f163d24b6ec77d
nitish <nitishkumarmay30@gmail.com>
2022-12-22 18:45:00 +0530

    Add solution for problem `C - Kefa and Park` Link: https://codeforces.com/contest/580/problem/C Tags: dfs and similar, graphs, trees, *1500 Ref: https://codeforces.com/contest/580/submission/186355834

---
+++ codeforces/580/C.py
@@ -0,0 +1,116 @@
+## My template code
+
+#bisect.bisect_left(a, x, lo=0, hi=len(a)) is the analog of std::lower_bound()
+#bisect.bisect_right(a, x, lo=0, hi=len(a)) is the analog of std::upper_bound()
+#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
+#from collections import deque as dq #deque  e.g. myqueue=dq(list)
+#append/appendleft/appendright/pop/popleft
+#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
+#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
+#import statistics as stat  # stat.median(a), mode, mean
+#from itertools import permutations(p,r)#combinations(p,r)
+#combinations(p,r) gives r-length tuples #combinations_with_replacement
+#every element can be repeated
+        
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+
+def bootstrap(f, stack=[]):
+    from types import GeneratorType
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+ 
+    return wrappedfunc
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+@bootstrap
+def dfs(u,p,numC,maxC,arr,adj,m):
+    global ans
+    if arr[u]:
+        numC+=1
+    else:
+        numC=0
+    
+    maxC=max(maxC,numC)
+
+    numChild=0
+
+    for v in adj[u]:
+        if v!=p:
+            yield dfs(v,u,numC,maxC,arr,adj,m)
+            numChild+=1
+    
+    if numChild==0 and maxC<=m:
+        ans+=1
+    yield ans
+
+def solve():
+    global ans
+    ans=0
+    n,m=mii()
+    arr=[-1]+lmii()
+
+    adj=defaultdict(list)
+    for j in range(n-1):
+        u,v=mii()
+        adj[u].append(v)
+        adj[v].append(u)
+
+    print(dfs(1,-1,0,0,arr,adj,m))
+
+
+    
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+ 
+    solve()
+ 
+ 
+if __name__ == '__main__':
+    main()
+    
+ 
