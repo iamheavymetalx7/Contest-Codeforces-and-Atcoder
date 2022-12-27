eca71ca7336c0e866e2346b8f6304b40909ea758
nitish <nitishkumarmay30@gmail.com>
2022-12-27 13:05:00 +0530

    Add solution for problem `B - Two Buttons` Link: https://codeforces.com/contest/520/problem/B Tags: dfs and similar, graphs, greedy, implementation, math, shortest paths, *1400 Ref: https://codeforces.com/contest/520/submission/186845119

---
+++ codeforces/520/B.py
@@ -0,0 +1,70 @@
+#bisect.bisect_left(a, x, lo=0, hi=len(a)) is the analog of std::lower_bound()
+#bisect.bisect_right(a, x, lo=0, hi=len(a)) is the analog of std::upper_bound()
+#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
+#from collections import deque as dq #deque  e.g. myqueue=dq(list)
+#append/appendleft/appendright/pop/popleft
+#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
+#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
+#import statistics as stat  # stat.median(a), mode, mean
+#from itertools import permutations(p,r)#combinations(p,r)
+#combinations(p,r) gives r-length tuples #combinations_with_replacement
+#every element can be repeated
+        
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+## codeforces assemble
+
+
+'''
+ we should get the number n starting 
+ from m using the operations "add 1 to the number" 
+ and "divide the number by 2 if it is even".
+'''
+def solve():
+    n,m=mii()
+    cnt=0
+    while n<m:
+        if m%2:
+            m+=1
+            cnt+=1
+        else:
+            m//=2
+            cnt+=1
+    print(cnt+(n-m))
+    
+    
+def main():
+    solve()
+ 
+ 
+if __name__ == '__main__':
+    main()
+    
+ 
+
+
