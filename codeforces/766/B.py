0a5c8364f1891436f7105dc125fa8e5bcdea3703
nitish <nitishkumarmay30@gmail.com>
2022-12-27 12:26:00 +0530

    Add solution for problem `B - Mahmoud and a Triangle` Link: https://codeforces.com/contest/766/problem/B Tags: constructive algorithms, geometry, greedy, math, number theory, sortings, *1000 Ref: https://codeforces.com/contest/766/submission/186842113

---
+++ codeforces/766/B.py
@@ -0,0 +1,68 @@
+#bisect.bisect_left(a, x, lo=0, hi=len(a)) is the analog of std::lower_bound()
+#bisect.bisect_right(a, x, lo=0, hi=len(a)) is the analog of std::upper_bound()
+#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
+#from collections import deque as dq #deque  e.g. myqueue=dq(list)
+#append/appendleft/appendright/pop/popleft
+#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
+#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
+#import statistics as stat  # stat.median(a), mode, mean
+#from itertools import permutations(p,r)#combinations(p,r)
+#combinations(p,r) gives r-length tuples #combinations_with_replacement
+#every element can be repeated
+        
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+def solve():
+    n=ii()
+    nums=lmii()
+    nums.sort()
+
+    # [a-----b---c] since c>=b and c>=a, cases like c+a>b
+    # c+b>a are trivially satisfied, only need to check
+    # a+b>c or not
+    ans=0
+    for i in range(1,n-1):
+        if nums[i-1]+nums[i]>nums[i+1]:
+            print("YES")
+            break
+    else:
+        print("NO")
+
+    
+def main():
+
+ 
+    solve()
+ 
+if __name__ == '__main__':
+    main()
+    
+ 
+
+
