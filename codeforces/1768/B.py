efd67507fb7d0666cf2243f6fd7fa6fe96eab362
nitish <nitishkumarmay30@gmail.com>
2023-01-06 08:47:00 +0530

    Add solution for problem `B - Quick Sort` Link: https://codeforces.com/contest/1768/problem/B Tags: greedy, math Ref: https://codeforces.com/contest/1768/submission/188158870

---
+++ codeforces/1768/B.py
@@ -0,0 +1,102 @@
+#bisect.bisect_left(a, x, lo=0, hi=len(a)) is the analog of std::lower_bound()
+#bisect.bisect_right(a, x, lo=0, hi=len(a)) is the analog of std::upper_bound()
+#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
+#from collections import deque as dq #deque  e.g. myqueue=dq(list)
+#append/appendleft/appendright/pop/popleft
+#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
+#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
+#import statistics as stat  # stat.median(a), mode, mean
+#from itertools import permutations(p,r)#combinations(p,r)
+#combinations(p,r) gives r-length tuples #combinations_with_replacement
+#every element can be repeated
+        
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+# Codeforces assemble 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+def solve(t):
+    #  permutation h, unsorted 
+    #  choose k element (distinct), work with them!!
+    #  1<=k<=n
+
+    n,k=mii()
+    perm=lmii()
+
+    # smallest = n+1
+    # ans=n+1
+
+    # for i in range(n-1,-1,-1):
+    #     if perm[i]>smallest:
+    #         ans=min(ans,perm[i])
+    #     smallest=min(perm[i],smallest)
+    # # print(ans,smallest)
+    # ans=n-ans+1
+
+    # if ans==n+1:
+    #     print(0)
+    # elif ans%k==0:
+    #     print(ans//k)
+    # else: print(ans//k+1)
+
+    prev=0
+    cnt=0
+
+    for i in range(n):
+        if perm[i]==prev+1:
+            prev=perm[i]
+        else:
+            cnt+=1
+
+    print((cnt+k-1)//k)
+
+
+
+
+    
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        solve(i+1)
+ 
+ 
+if __name__ == '__main__':
+    main()
+    
+ 
+
+
