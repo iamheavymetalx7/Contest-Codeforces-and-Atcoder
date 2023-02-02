b639a2c2be14eee10262955ebe3f53251f610063
nitish <nitishkumarmay30@gmail.com>
2023-02-02 13:32:00 +0530

    Add solution for problem `C - Flexible String` Link: https://codeforces.com/contest/1778/problem/C Tags: bitmasks, brute force, strings Ref: https://codeforces.com/contest/1778/submission/191674084

---
+++ codeforces/1778/C.py
@@ -0,0 +1,98 @@
+#bisect.bisect_left(a, x, lo=0, hi=len(a)) is the analog of std::lower_bound()
+#bisect.bisect_right(a, x, lo=0, hi=len(a)) is the analog of std::upper_bound()
+#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
+#from collections import deque as dq #deque  e.g. myqueue=dq(list)
+#append/appendleft/appendright/pop/popleft
+#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
+#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
+#import statistics as stat  # stat.median(a), mode, mean
+#from itertools import permutations(p,r)#combinations(p,r)
+#combinations(p,r) gives r-length tuples #combinations_with_replacement
+#every element can be repeated
+        
+import sys, threading, os, io 
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+from bisect import bisect_left as lower_bound
+from bisect import bisect_right as upper_bound
+from io import BytesIO, IOBase								
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
+#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+from itertools import combinations
+from collections import Counter
+from heapq import heapify, heappush, heappop
+def solve(t):
+    n,k=mii()
+    a=si()
+    b=si()
+    l=list(set(a)) ## contains distinct characters of a
+
+
+    ans=0
+    # time complexity  =  O(N* (dictinct chars in a choose m))
+
+    for subset in combinations(l,min(k,len(l))):
+        subset=set(subset)
+        cnt=0
+        curans=0
+        for i in range(n):
+            if a[i]==b[i] or a[i] in subset:
+                cnt+=1
+            else:
+                curans += ((cnt)*(cnt+1))//2
+                cnt=0
+        curans+= ((cnt)*(cnt+1))//2
+        ans=max(ans, curans)
+    
+    print(ans)
+
+
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        solve(i+1)
+ 
+ 
+if __name__ == '__main__':
+    main()
+    
+ 
+
+
