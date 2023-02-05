813173be2871c000b38f28ef8efeda46625faacc
nitish <nitishkumarmay30@gmail.com>
2023-02-05 13:42:00 +0530

    Add solution for problem `C - Product of Three Numbers` Link: https://codeforces.com/contest/1294/problem/C Tags: greedy, math, number theory, *1300 Ref: https://codeforces.com/contest/1294/submission/192246326

--- codeforces/1294/C.py
+++ codeforces/1294/C.py
@@ -59,33 +59,139 @@ c2i = lambda c: ord(c) - ord('a')
 # else:
 #     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
     
-## ayush chabra's solution, better and shorter than mine
-import math         
-def solve(t):
 
+
+
+############################################################    
+
+from collections import Counter
+
+
+def gcd(x, y):
+    """greatest common divisor of x and y"""
+    while y:
+        x, y = y, x % y
+    return x
+
+
+def memodict(f):
+    """memoization decorator for a function taking a single argument"""
+    class memodict(dict):
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret
+
+    return memodict().__getitem__
+
+
+def pollard_rho(n):
+    """returns a random factor of n"""
+    if n & 1 == 0:
+        return 2
+    if n % 3 == 0:
+        return 3
+
+    s = ((n - 1) & (1 - n)).bit_length() - 1
+    d = n >> s
+    for a in [2, 325, 9375, 28178, 450775, 9780504, 1795265022]:
+        p = pow(a, d, n)
+        if p == 1 or p == n - 1 or a % n == 0:
+            continue
+        for _ in range(s):
+            prev = p
+            p = (p * p) % n
+            if p == 1:
+                return gcd(prev - 1, n)
+            if p == n - 1:
+                break
+        else:
+            for i in range(2, n):
+                x, y = i, (i * i + 1) % n
+                f = gcd(abs(x - y), n)
+                while f == 1:
+                    x, y = (x * x + 1) % n, (y * y + 1) % n
+                    y = (y * y + 1) % n
+                    f = gcd(abs(x - y), n)
+                if f != n:
+                    return f
+    return n
+
+
+@memodict
+def prime_factors(n):
+    """returns a Counter of the prime factorization of n"""
+    if n <= 1:
+        return Counter()
+    f = pollard_rho(n)
+    return Counter([n]) if f == n else prime_factors(f) + prime_factors(n // f)
+
+
+def distinct_factors(n):
+    """returns a list of all distinct factors of n"""
+    factors = [1]
+    for p, exp in prime_factors(n).items():
+        factors += [p**i * factor for factor in factors for i in range(1, exp + 1)]
+    return factors
+
+
+def all_factors(n):
+    """returns a sorted list of all distinct factors of n"""
+    small, large = [], []
+    for i in range(1, int(n**0.5) + 1, 2 if n & 1 else 1):
+        if not n % i:
+            small.append(i)
+            large.append(n // i)
+    if small[-1] == large[-1]:
+        large.pop()
+    large.reverse()
+    small.extend(large)
+    return small
+############################################################    
+  
+def solve(t):
     n=ii()
 
-    ans=[]
+    arr=[1]*3
+    cnt_v=0
+    cnt_k=0
 
-    for ele in range(2,int(math.sqrt(n))+1):
-        if len(ans)==2:
-            break
-        if n%ele==0:
-            n=n//ele
-            ans.append(ele)
-        
-    ans.append(n)
+    p,q,r=True,True,True
 
-    if len(ans)==3 and len(set(ans))==3:
-        print("Yes")
-        print(*ans)
+    for key,val in prime_factors(n).items():
+        # print(key,val)
+        cnt_v+=val
+        cnt_k+=1
+
+    if (cnt_k>=2 and cnt_v>=4) or cnt_v>5 or cnt_k>=3:
+        print("YES")
+
+        if cnt_k>=2:
+            for key,val in prime_factors(n).items():
+                if p:
+                    arr[0]*=key
+                    if val-1>=0:
+                        arr[2]*=pow(key,val-1)
+                    p=False
+                elif q:
+                    arr[1]*=key
+                    if val-1>=0:
+                        arr[2]*=pow(key,val-1)
+                    q=False
+                elif r:
+                    arr[2]*=pow(key,val)
+        else:
+            if p:
+                arr[0]*=key
+                arr[1]*=pow(key,2)
+                arr[2]*=pow(key,val-3)
+
+        print(*arr)          
+
+
+
+
     else:
-        print("No")
-
-
-
-
-
+        print("NO")
 
     
 def main():
