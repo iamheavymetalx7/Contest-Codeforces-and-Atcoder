7df5596b4dea53915c0a54ddd402d9f356372fc6
nitish <nitishkumarmay30@gmail.com>
2023-02-07 11:19:00 +0530

    Add solution for problem `C - Card Game` Link: https://codeforces.com/contest/1739/problem/C Tags: combinatorics, constructive algorithms, dp, games, *1500 Ref: https://codeforces.com/contest/1739/submission/192559766

---
+++ codeforces/1739/C.py
@@ -0,0 +1,124 @@
+#bisect.bisect_left(a, x, lo=0, hi=len(a)) is the analog of std::lower_bound()
+#bisect.bisect_right(a, x, lo=0, hi=len(a)) is the analog of std::upper_bound()
+#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
+#from collections import deque as dq #deque  e.g. myqueue=dq(list)
+#append/appendleft/appendright/pop/popleft
+#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
+#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
+#import statistics as stat  # stat.median(a), mode, mean
+#from itertools import permutations(p,r)#combinations(p,r)
+#combinations(p,r) gives r-length tuples #combinations_with_replacement
+#every element can be repeated
+        
+import sys, threading, os, io 
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+from bisect import bisect_left as lower_bound
+from bisect import bisect_right as upper_bound
+from io import BytesIO, IOBase								
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+input = lambda: sys.stdin.readline().rstrip(
+)
+def lmii():
+    return list(map(int,input().split()))
+
+def ii():
+    return int(input())
+
+def si():
+    return str(input())
+def lmsi():
+    return list(map(str,input().split()))
+def mii():
+    return map(int,input().split())
+
+def msi():
+    return map(str,input().split())
+
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+def make_nCr_mod(max_n=100, mod=998244353):
+    max_n = min(max_n, mod - 1)
+ 
+    fact, inv_fact = [0] * (max_n + 1), [0] * (max_n + 1)
+    fact[0] = 1
+    for i in range(max_n):
+        fact[i + 1] = fact[i] * (i + 1) % mod
+ 
+    inv_fact[-1] = pow(fact[-1], mod - 2, mod)
+    for i in reversed(range(max_n)):
+        inv_fact[i] = inv_fact[i + 1] * (i + 1) % mod
+ 
+    def nCr_mod(n, r):
+        res = 1
+        while n or r:
+            a, b = n % mod, r % mod
+            if a < b:
+                return 0
+            res = res * fact[a] % mod * inv_fact[b] % mod * inv_fact[a - b] % mod
+            n //= mod
+            r //= mod
+        return res
+ 
+    return nCr_mod
+
+def solve(t):
+    ncr=  make_nCr_mod()
+    n=ii()
+
+    alice=[0]*61
+    boris=[0]*61
+
+    alice[2]=1
+
+    mod = 998244353
+
+    for i in range (4, 61, 2):
+        tot = ncr(i, i//2)
+        alice[i] = ncr((i-1), (i-2)//2) + boris[i-2]
+        boris[i] = tot - alice[i] - 1
+        alice[i]%=mod
+        boris[i]%=mod
+ 
+    print(alice[n],boris[n],1)
+
+    
+    
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        solve(i+1)
+ 
+ 
+if __name__ == '__main__':
+    main()
+    
+ 
+
+
