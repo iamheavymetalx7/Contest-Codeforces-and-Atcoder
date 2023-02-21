3342e1c32f5a0a91a87df34fe0425503e6711f33
nitish <nitishkumarmay30@gmail.com>
2023-02-21 16:31:00 +0530

    Add solution for problem `B - Random Teams` Link: https://codeforces.com/contest/478/problem/B Tags: combinatorics, constructive algorithms, greedy, math, *1300 Ref: https://codeforces.com/contest/478/submission/194443647

--- codeforces/478/B.py
+++ codeforces/478/B.py
@@ -1,12 +1,87 @@
-n, m = map(int, input().split())
+#bisect.bisect_left(a, x, lo=0, hi=len(a)) is the analog of std::lower_bound()
+#bisect.bisect_right(a, x, lo=0, hi=len(a)) is the analog of std::upper_bound()
+#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
+#from collections import deque as dq #deque  e.g. myqueue=dq(list)
+#append/appendleft/appendright/pop/popleft
+#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
+#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
+#import statistics as stat  # stat.median(a), mode, mean
+#from itertools import permutations(p,r)#combinations(p,r)
+#combinations(p,r) gives r-length tuples #combinations_with_replacement
+#every element can be repeated
+        
+#Note direct assignment to check somethings doesnt work always
+#say there exists s (list) then ss=s and if we edit ss, it edits s as well
+#always try to use ss=s.copy() if u wish to make changes to ss and not reflect them in s.
+#For example: see **1379A - Acacius and String** for reference
+    
+import sys, threading, os, io 
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+from bisect import bisect_left as lower_bound
+from bisect import bisect_right as upper_bound
+from io import BytesIO, IOBase								
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
  
-Max = n - (m-1)
-Max = (Max*(Max-1))//2
  
-x = n//m
-Min = ((x*(x-1))//2)*m
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
  
-Min += (n%m)*x
  
-print(Min, end=" ")
-print(Max)
+input = lambda: sys.stdin.readline().rstrip(
+)
+def lmii():
+    return list(map(int,input().split()))
+
+def ii():
+    return int(input())
+
+def si():
+    return str(input())
+def lmsi():
+    return list(map(str,input().split()))
+def mii():
+    return map(int,input().split())
+
+def msi():
+    return map(str,input().split())
+
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
+#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+from math import comb
+n,m=mii()
+
+new =n 
+
+maxi2=n-(m-1)
+# print(maxi1,maxi2)
+
+to_ret = comb(maxi2,2)
+
+n=new
+gp=n//m
+rem=n-gp*m
+# print(n,gp,m)
+kmin=(gp*(gp-1)*(m-rem)//2)+rem*(gp+1)*(gp)//2
+
+
+
+
+
+print(kmin,to_ret)
