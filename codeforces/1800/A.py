443db36f868244d6b62791a8e7a9b411c3da458f
nitish <nitishkumarmay30@gmail.com>
2023-03-02 20:38:00 +0530

    Add solution for problem `A - Is It a Cat?` Link: https://codeforces.com/contest/1800/problem/A Tags: implementation, strings Ref: https://codeforces.com/contest/1800/submission/195619164

--- codeforces/1800/A.py
+++ codeforces/1800/A.py
@@ -1,12 +1,91 @@
-for _ in range(int(input())):
-    n = int(input())
-    s = input().lower()
-    l = '#'
-    for x in s:
-        if x != l[-1]:
-            l += x
+
+import sys, threading, os, io 
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+from bisect import bisect_left as lower_bound
+from bisect import bisect_right as upper_bound
+from io import BytesIO, IOBase								
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
  
-    if l == '#meow':
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+input = lambda: sys.stdin.readline().rstrip(
+)
+def lmii():
+    return list(map(int,input().split()))
+
+def ii():
+    return int(input())
+
+def si():
+    return str(input())
+def lmsi():
+    return list(map(str,input().split()))
+def mii():
+    return map(int,input().split())
+
+def msi():
+    return map(str,input().split())
+
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
+#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+    
+    
+def solve():
+    n=ii()
+    s=list(si())
+
+
+    if s[0].lower()!="m":
+        print("NO")
+        return
+    if s[-1].lower()!="w":
+        print("NO")
+        return
+    arr=[]
+    i=0
+    for i in range(n-1):
+        if s[i].lower()!=s[i+1].lower():
+            arr.append(s[i].lower())
+    
+    if arr[-1]!="w":
+        arr.append(s[-1].lower())
+    # print(arr)
+    if "".join(arr)=="meow":
         print("YES")
     else:
-        print('NO') 
+        print("NO")    
+
+
+
+        
+
+
+
+
+
+
+t=int(input())
+for _ in range(t):
+    solve()
+ 
+
+
