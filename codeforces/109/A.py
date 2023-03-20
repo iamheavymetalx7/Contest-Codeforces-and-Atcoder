93a0650bd670c4fae8cd6db7369a1c3d119b5328
nitish <nitishkumarmay30@gmail.com>
2023-03-20 14:49:00 +0530

    Add solution for problem `A - Lucky Sum of Digits` Link: https://codeforces.com/contest/109/problem/A Tags: brute force, implementation, *1000 Ref: https://codeforces.com/contest/109/submission/198336502

---
+++ codeforces/109/A.py
@@ -0,0 +1,107 @@
+#bisect.bisect_left(a, x, lo=0, hi=len(a)) is the analog of std::lower_bound()
+#bisect.bisect_right(a, x, lo=0, hi=len(a)) is the analog of std::upper_bound()
+#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
+#from collections import deque as dq #deque  e.g. myqueue=dq(list)
+#append/appendleft/appendright/pop/popleft
+#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
+#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
+#import statistics as stat  # stat.median(a), mode, mean
+#from itertools import permutations(p,r)#combinations(p,r)
+#combinations(p,r) gives r-length tuples #combinations_with_replacement
+#every element can be repeated
+        
+#Note direct assignment to check somethings doesnt work always
+#say there exists s (list) then ss=s and if we edit ss, it edits s as well
+#always try to use ss=s.copy() if u wish to make changes to ss and not reflect them in s.
+#For example: see **1379A - Acacius and String** for reference
+    
+import sys, threading, os, io 
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+from bisect import bisect_left as lower_bound
+from bisect import bisect_right as upper_bound
+from io import BytesIO, IOBase								
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+input = lambda: sys.stdin.readline().rstrip(
+)
+def lmii():
+    return list(map(int,input().split()))
+
+def ii():
+    return int(input())
+
+def si():
+    return str(input())
+def lmsi():
+    return list(map(str,input().split()))
+def mii():
+    return map(int,input().split())
+
+def msi():
+    return map(str,input().split())
+
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
+#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+    
+    
+def solve():
+    n=ii()
+    till=n//7
+    ele=10**9
+    x,y=10**5,10**5
+    arr=[]
+    for b in range(till+1):
+        a= (n-b*7)//4
+
+        if a*4+7*b==n:
+            if a+b<=ele:
+                arr.append([a,b])
+    if not arr:
+        print(-1)
+        return
+    arr.sort(key=lambda p:p[0]+p[1])
+    a=arr[0][0]
+    b=arr[0][1]
+    for i in range(1,len(arr)):
+        if arr[i][0]+arr[i][1]>arr[0][0]+arr[0][1]:
+            break
+        if b>arr[i][1]:
+            a=arr[i][0]
+            b=arr[i][1]
+
+    if n==a*4+b*7:
+        print("4"*a+"7"*b)
+    else:
+        print(-1)    
+
+
+    
+
+                
+
+    
+    
+    
+    
+solve()
