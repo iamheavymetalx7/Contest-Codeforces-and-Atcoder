7166b8f3d52973a22c26e5ac1487e093e5aad82a
nitish <nitishkumarmay30@gmail.com>
2023-03-22 10:00:00 +0530

    Add solution for problem `C - Xenia and Weights` Link: https://codeforces.com/contest/339/problem/C Tags: constructive algorithms, dfs and similar, dp, graphs, greedy, shortest paths, *1700 Ref: https://codeforces.com/contest/339/submission/198574702

--- codeforces/339/C.py
+++ codeforces/339/C.py
@@ -1,43 +1,96 @@
-import sys
-sys.setrecursionlimit(10**5)
-l=list(input())
-m=int(input())
-ans=[]
-def fun(step,balance,weight):
-    if step==m:
-        return 1
-    if balance>0:
-        for i in range(len(l)):
-            if i+1==weight:
-                continue
-            if l[i]=="1" and balance-i-1<0:
-                ans.append(i+1)
-                if fun(step+1,balance-i-1,i+1):
-                    return 1
-                ans.pop()
-        return 0
-    if balance<0:
-        for i in range(len(l)):
-            if i+1==weight:
-                continue
-            if (l[i]=="1" and balance+i+1>0):
-                ans.append(i+1)
-                if fun(step+1,balance+i+1,i+1):
-                    return 1
-                ans.pop()
-        return 0
-    if balance==0:
-        for i in range(len(l)):
-            if l[i]=="1":
-                ans.append(i+1)
-                if fun(step+1,i+1,i+1):
-                    return 1
-                ans.pop()
-        return 0
-if fun(0,0,0):
-    print("YES")
-    print(*ans)
-else :
-    print("NO")
+#bisect.bisect_left(a, x, lo=0, hi=len(a)) is the analog of std::lower_bound()
+#bisect.bisect_right(a, x, lo=0, hi=len(a)) is the analog of std::upper_bound()
+#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
+#from collections import deque as dq #deque  e.g. myqueue=dq(list)
+#append/appendleft/appendright/pop/popleft
+#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
+#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
+#import statistics as stat  # stat.median(a), mode, mean
+#from itertools import permutations(p,r)#combinations(p,r)
+#combinations(p,r) gives r-length tuples #combinations_with_replacement
+#every element can be repeated
+        
+#Note direct assignment to check somethings doesnt work always
+#say there exists s (list) then ss=s and if we edit ss, it edits s as well
+#always try to use ss=s.copy() if u wish to make changes to ss and not reflect them in s.
+#For example: see **1379A - Acacius and String** for reference
+    
+import sys, threading, os, io 
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+from bisect import bisect_left as lower_bound
+from bisect import bisect_right as upper_bound
+from io import BytesIO, IOBase								
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+input = lambda: sys.stdin.readline().rstrip()
+def lmii():
+    return list(map(int,input().split()))
+
+def ii():
+    return int(input())
+
+def si():
+    return str(input())
+def lmsi():
+    return list(map(str,input().split()))
+def mii():
+    return map(int,input().split())
+
+def msi():
+    return map(str,input().split())
+
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
+#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+    
+    
+def solve():
+    s=si()
+    m=ii()
+
+    arr=[]
+    for i in range(len(s)):
+        if s[i]=="1":
+            arr.append(i+1)
+    # print(arr)
+    q=[[0,-1,0,[]]]  
+    flag=True
+    while q:
+        curr_val, last, idx, path = q.pop()
+        # print(curr_val,last,idx,path)
+        if idx==m:
+            print("YES")
+            flag=False
+            print(*path)
+            break
+        
+        for i in arr:
+            # print(i,"iiiiii",i!=last, i>curr_val)
+            if i!=last and i>curr_val:
+                q.append([i-curr_val,i,idx+1,path+[i]])
+    if flag:
+        print("NO")
 
     
+    
+solve()
