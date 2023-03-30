0b7613cf8a3593ebf0d431f70bb3d43b27fa7f89
nitish <nitishkumarmay30@gmail.com>
2023-03-30 10:40:00 +0530

    Add solution for problem `E - Binary Inversions` Link: https://codeforces.com/contest/1760/problem/E Tags: data structures, greedy, math, *1100 Ref: https://codeforces.com/contest/1760/submission/199782424

---
+++ codeforces/1760/E.py
@@ -0,0 +1,111 @@
+#bisect.bisect_left(a, x, lo=0, hi=len(a)) is the analog of std::lower_bound()
+#bisect.bisect_right(a, x, lo=0, hi=len(a)) is the analog of std::upper_bound()
+#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
+#from collections import deque as dq #deque  e.g. myqueue=dq(list)
+#append/appendleft/appendright/pop/popleft
+#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
+#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
+#import statistics as stat  # stat.median(a), mode, mean
+#from itertools import permutations(p,r)#combinations(p,r)
+#combinations(p,r) gives r-length tuples #combinations_with_replacement
+#every element can be repeated
+        
+#Note direct assignment to check somethings doesnt work always
+#say there exists s (list) then ss=s and if we edit ss, it edits s as well
+#always try to use ss=s.copy() if u wish to make changes to ss and not reflect them in s.
+#For example: see **1379A - Acacius and String** for reference
+    
+import sys, threading, os, io 
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+from bisect import bisect_left as lower_bound
+from bisect import bisect_right as upper_bound
+from io import BytesIO, IOBase								
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+input = lambda: sys.stdin.readline().rstrip()
+def lmii():
+    return list(map(int,input().split()))
+
+def ii():
+    return int(input())
+
+def si():
+    return str(input())
+def lmsi():
+    return list(map(str,input().split()))
+def mii():
+    return map(int,input().split())
+
+def msi():
+    return map(str,input().split())
+
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
+#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+    
+    
+def solve():
+    n=ii()
+    a=lmii()
+    
+    initial_inversion=0
+    cnt=0
+    max_pos_one=-1
+    min_pos_zero=10**9
+    for i in range(n):
+        if a[i]==1:
+            max_pos_one=max(max_pos_one,i)
+            cnt+=1
+        if a[i]==0:
+            min_pos_zero=min(min_pos_zero,i)
+            initial_inversion+=cnt
+    # print(initial_inversion)
+    
+    b=a.copy()
+
+    new_inv=0
+    if min_pos_zero<10**9:
+        b[min_pos_zero]=1
+        cnt=0
+        for i in range(n):
+            if b[i]==1:
+                cnt+=1
+            if b[i]==0:
+                new_inv+=cnt
+    new_inv2=0
+   
+    if max_pos_one>-1:
+        a[max_pos_one]=0
+        cnt=0
+        for i in range(n):
+            if a[i]==1:
+                cnt+=1
+            if a[i]==0:
+                new_inv2+=cnt
+    
+    print(max(new_inv,new_inv2,initial_inversion))
+    
+    
+t=ii()
+for _ in range(t):
+    solve()
