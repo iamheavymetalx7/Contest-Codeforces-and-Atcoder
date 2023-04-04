e995a7732c2cb3f22fdfdda330d37c6ab145bcc3
nitish <nitishkumarmay30@gmail.com>
2023-04-04 10:03:00 +0530

    Add solution for problem `C - Permutation Operations` Link: https://codeforces.com/contest/1746/problem/C Tags: constructive algorithms, greedy, implementation, math, *1300 Ref: https://codeforces.com/contest/1746/submission/200636038

--- codeforces/1746/C.py
+++ codeforces/1746/C.py
@@ -1,37 +1,29 @@
-# import os,sys,io, threading
-# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
-#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
-#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
-# else:
-#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
-    
-    
 
 def solve():
     n=int(input())
     a=list(map(int, input().split()))
 
+
     
     diffs=[]
+    ans=[]
 
     for i in range(n-1):
         if a[i]>a[i+1]:
             diffs.append((a[i]-a[i+1],i+1))
     
     diffs.sort()
-
-    s=1
-    ans=[]
+    k=1
     for ele in diffs:
         x=ele[0]
         while x>0:
-            x-=s
-            s+=1
             ans.append(ele[1]+1)
-
+            x-=k
+            k+=1
     
     if len(ans)!=n:
         ans.extend([n]*(n-len(ans)))
+    
 
     print(*ans)
 
