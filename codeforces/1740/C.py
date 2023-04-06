a0d171d88637e9b6a53b81087205a47c08cc0b90
nitish <nitishkumarmay30@gmail.com>
2023-04-06 12:47:00 +0530

    Add solution for problem `C - Bricks and Bags` Link: https://codeforces.com/contest/1740/problem/C Tags: constructive algorithms, games, greedy, sortings, *1400 Ref: https://codeforces.com/contest/1740/submission/200945639

---
+++ codeforces/1740/C.py
@@ -0,0 +1,75 @@
+#  see **1379A - Acacius and String** for reference
+'''
+tutorial
+'''  
+import sys, threading, os, io 
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+from bisect import bisect_left as lower_bound
+from bisect import bisect_right as upper_bound
+from io import BytesIO, IOBase								
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+input = lambda: sys.stdin.readline().rstrip()
+def lmii():
+    return list(map(int,input().split()))
+
+def ii():
+    return int(input())
+
+def si():
+    return str(input())
+def lmsi():
+    return list(map(str,input().split()))
+def mii():
+    return map(int,input().split())
+
+def msi():
+    return map(str,input().split())
+
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
+#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+    
+    
+def solve():
+    n=ii()
+    a=lmii()
+    a.sort()
+
+    ans=0
+    ## if we assign block2 on left
+    for i in range(n-1):
+        ans=max(ans,a[i+1]-a[i]+a[-1]-a[i])
+
+    ## if we assign block2 on right
+    for i in range(1,n-1):
+        ans=max(ans,a[i+1]-a[0]+a[i+1]-a[i])
+    
+    print(ans)
+
+    
+    
+    
+t=ii()
+for _ in range(t):
+    solve()
