1f6ed8b25aa4c5082b7bc99d5cf079fa788249f2
nitish <nitishkumarmay30@gmail.com>
2023-04-17 09:46:00 +0530

    Add solution for problem `C - Jatayu's Balanced Bracket Sequence` Link: https://codeforces.com/contest/1726/problem/C Tags: data structures, dsu, graphs, greedy, *1300 Ref: https://codeforces.com/contest/1726/submission/202438278

---
+++ codeforces/1726/C.py
@@ -0,0 +1,71 @@
+'''
+Claim: The answer is one more than the number of occurrences
+ of the substring "((" in the balanced bracket sequence 
+ given (considering overlapping occurrences as well).
+'''
+    
+import sys, threading, os, io 
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+from bisect import bisect_left as lower_bound
+from bisect import bisect_right as upper_bound
+from io import BytesIO, IOBase								
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+input = lambda: sys.stdin.readline().rstrip()
+def lmii():
+    return list(map(int,input().split()))
+
+def ii():
+    return int(input())
+
+def si():
+    return str(input())
+def lmsi():
+    return list(map(str,input().split()))
+def mii():
+    return map(int,input().split())
+
+def msi():
+    return map(str,input().split())
+
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
+#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+    
+    
+def solve():
+    n=ii()
+    n*=2
+    s=si()
+    cnt=0
+    for i in range(n):
+        if s[i]=="(":
+            if i+1<n and s[i+1]=="(":
+                cnt+=1
+    print(cnt+1)
+    
+    
+    
+t=ii()
+for _ in range(t):
+    solve()
