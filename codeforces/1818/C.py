3f09f1a36c3ed0828461a486a2461757b720f925
nitish <nitishkumarmay30@gmail.com>
2023-04-29 21:30:00 +0530

    Add solution for problem `C - Almost Increasing Subsequence` Link: https://codeforces.com/contest/1818/problem/C Tags: binary search, dp, greedy, *1500 Ref: https://codeforces.com/contest/1818/submission/203944269

--- codeforces/1818/C.py
+++ codeforces/1818/C.py
@@ -1,29 +1,120 @@
-# ﷽
-from itertools import accumulate
-import sys
-input = lambda: sys.stdin.readline().strip()
-mod=7+10**9
-def inlst():return [int(i) for i in input().split()]
+# /**
+# * author:Hisoka-TheMagician
+# * created: 29/04/2023 20:19 Chennai, India
+# **/
+        
 
+
+
+
+#bisect.bisect_left(a, x, lo=0, hi=len(a)) is the analog of std::lower_bound()
+#bisect.bisect_right(a, x, lo=0, hi=len(a)) is the analog of std::upper_bound()
+#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
+#from collections import deque as dq #deque  e.g. myqueue=dq(list)
+#append/appendleft/appendright/pop/popleft
+#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
+#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
+#import statistics as stat  # stat.median(a), mode, mean
+#from itertools import permutations(p,r)#combinations(p,r)
+#combinations(p,r) gives r-length tuples #combinations_with_replacement
+#every element can be repeated
+        
+#Note direct assignment to check somethings doesnt work always
+#say there exists s (list) then ss=s and if we edit ss, it edits s as well
+#always try to use ss=s.copy() if u wish to make changes to ss and not reflect them in s.
+#For example: see **1379A - Acacius and String** for reference
+    
+import sys, threading, os, io 
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+from bisect import bisect_left as lower_bound
+from bisect import bisect_right as upper_bound
+from io import BytesIO, IOBase								
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+input = lambda: sys.stdin.readline().rstrip()
+def lmii():
+    return list(map(int,input().split()))
+
+def ii():
+    return int(input())
+
+def si():
+    return str(input())
+def lmsi():
+    return list(map(str,input().split()))
+def mii():
+    return map(int,input().split())
+
+def msi():
+    return map(str,input().split())
+
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
+#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+    
+    
 def solve():
-    n,q=inlst()
-    lst=inlst()
-    dp=[0]*n
+    n,q=mii()
+    a=lmii()
+
+    prefix=[1,1]
     for i in range(2,n):
-          if lst[i]<=lst[i-1] and lst[i-1]<=lst[i-1-1]:dp[i]=1
-    dp=[0]+list(accumulate(dp))
-    # print(dp)
-    for i in range(q):
-          a,b=inlst()
+        if a[i-1]>=a[i] and a[i-2]>=a[i-1]:
+            prefix.append(-1)
+        else:
+            prefix.append(1)
+    # print(prefix)
 
-          if a==b:print(1)
-          elif a+1==b:print(2)
-          else:
-                # print(b-a+1,(dp[b]-dp[a]))
-                print(b-a+1-(dp[b]-dp[a+1]))
+    a=[]
+    c=0
+    for i in range(len(prefix)):
+        if prefix[i]==1:
+            c+=1
+            a.append(c)
+        else:
+            a.append(c)
+    # print(a)
+
+    for _ in range(q):
+        l,r=mii()
+        if r-l<2:
+            print(r-l+1)
+            continue
+
+
+        l-=1
+        r-=1
+
+        print(abs(prefix[l])+abs(prefix[l+1])+a[r]-a[l+1])
+    
 
 
 
-if __name__ == "__main__":
-    # for i in range(int(input())):
-        solve()
+
+    
+    
+    
+    
+t=1
+for _ in range(t):
+    solve()
