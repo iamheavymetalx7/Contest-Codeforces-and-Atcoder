1dbb8badb65db7f3ab7b835cd21cbfe3070d7cfb
nitish <nitishkumarmay30@gmail.com>
2023-05-03 20:15:00 +0530

    Add solution for problem `B - Swaps` Link: https://codeforces.com/contest/1573/problem/B Tags: greedy, math, sortings, *1400 Ref: https://codeforces.com/contest/1573/submission/204347094

---
+++ codeforces/1573/B.py
@@ -0,0 +1,81 @@
+# /**
+# * author:Hisoka-TheMagician
+# * created: 03/05/2023 19:18 Chennai, India
+# **/
+        
+
+
+import sys, threading, os, io 
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+from bisect import bisect_left as lower_bound
+from bisect import bisect_right as upper_bound
+from io import BytesIO, IOBase								
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+input = lambda: sys.stdin.readline().rstrip()
+def lmii():
+    return list(map(int,input().split()))
+
+def ii():
+    return int(input())
+
+def si():
+    return str(input())
+def lmsi():
+    return list(map(str,input().split()))
+def mii():
+    return map(int,input().split())
+
+def msi():
+    return map(str,input().split())
+
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
+#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+    
+    
+def solve():
+    n=ii()
+    a=lmii()
+    b=lmii()
+    dic=defaultdict(int)
+    for i in range(n):
+        dic[a[i]]=i
+    j=0
+    c=0
+    ans=n
+    for i in range(1,2*n,2):
+        while b[j]<i:
+            j+=1
+        c=j+dic[i]
+        ans=min(c,ans)
+    print(ans)
+    
+
+    
+    
+    
+    
+t=ii()
+for _ in range(t):
+    solve()
