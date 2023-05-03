65e98fe66cc064ef9bfc91c48f3155621d6d4e0a
nitish <nitishkumarmay30@gmail.com>
2023-05-03 20:15:00 +0530

    Add solution for problem `B - Swaps` Link: https://codeforces.com/contest/1573/problem/B Tags: greedy, math, sortings, *1400 Ref: https://codeforces.com/contest/1573/submission/204347062

--- codeforces/1573/B.py
+++ codeforces/1573/B.py
@@ -1,81 +1,27 @@
-# /**
-# * author:Hisoka-TheMagician
-# * created: 03/05/2023 19:18 Chennai, India
-# **/
-        
+import sys
+input = sys.stdin.readline
+import bisect
 
 
-import sys, threading, os, io 
-import math
-import time
-from os import path
-from collections import defaultdict, Counter, deque
-from bisect import *
-from string import ascii_lowercase
-from functools import cmp_to_key
-import heapq
-from bisect import bisect_left as lower_bound
-from bisect import bisect_right as upper_bound
-from io import BytesIO, IOBase								
-# # # # # # # # # # # # # # # #
-#       JAI SHREE RAM         #
-# # # # # # # # # # # # # # # #
- 
- 
-def lcm(a, b):
-    return (a*b)//(math.gcd(a,b))
- 
- 
-input = lambda: sys.stdin.readline().rstrip()
-def lmii():
-    return list(map(int,input().split()))
+t=int(input())
+for tests in range(t):
+    n=int(input())
+    A=list(map(int,input().split()))
+    B=list(map(int,input().split()))
 
-def ii():
-    return int(input())
+    for i in range(1,n):
+        B[i]=max(B[i-1],B[i])
 
-def si():
-    return str(input())
-def lmsi():
-    return list(map(str,input().split()))
-def mii():
-    return map(int,input().split())
+    ANS=1<<30
 
-def msi():
-    return map(str,input().split())
-
-i2c = lambda n: chr(ord('a') + n)
-c2i = lambda c: ord(c) - ord('a')
-    
-    
-# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
-#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
-#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
-# else:
-#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
-    
-    
-def solve():
-    n=ii()
-    a=lmii()
-    b=lmii()
-    dic=defaultdict(int)
     for i in range(n):
-        dic[a[i]]=i
-    j=0
-    c=0
-    ans=n
-    for i in range(1,2*n,2):
-        while b[j]<i:
-            j+=1
-        c=j+dic[i]
-        ans=min(c,ans)
-    print(ans)
-    
+        k=A[i]
+
+        x=bisect.bisect(B,k)
+
+        ANS=min(ANS,x+i)
+
+
+    print(ANS)
 
     
-    
-    
-    
-t=ii()
-for _ in range(t):
-    solve()
