202cfb40d620b269f40032198239acf37c2a2d59
nitish <nitishkumarmay30@gmail.com>
2023-05-05 10:57:00 +0530

    Add solution for problem `C - PolandBall and Forest` Link: https://codeforces.com/contest/755/problem/C Tags: dfs and similar, dsu, graphs, interactive, trees, *1300 Ref: https://codeforces.com/contest/755/submission/204523606

---
+++ codeforces/755/C.py
@@ -0,0 +1,80 @@
+# /**
+# * author:Hisoka-TheMagician
+# * created: 05/05/2023 10:49 Chennai, India
+# **/
+'''
+
+EDITORIAL
+EDITORIAL
+
+'''
+    
+import sys, threading, os, io 
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+from bisect import bisect_left as lower_bound
+from bisect import bisect_right as upper_bound
+from io import BytesIO, IOBase								
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+input = lambda: sys.stdin.readline().rstrip()
+def lmii():
+    return list(map(int,input().split()))
+
+def ii():
+    return int(input())
+
+def si():
+    return str(input())
+def lmsi():
+    return list(map(str,input().split()))
+def mii():
+    return map(int,input().split())
+
+def msi():
+    return map(str,input().split())
+
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
+#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+    
+    
+def solve():
+    n=ii()
+    a=lmii()
+    s=set()
+    ans=0
+
+    for i,j in enumerate(a):
+        if j==i+1:
+            ans+=1
+        else:
+            s.add(j)
+    ans+=len(s)//2
+
+    print(ans)
+    
+    
+    
+xxx=1
+for _ in range(xxx):
+    solve()
