a87685c2a3b1b50b353a0b286546a8b0be4140dd
nitish <nitishkumarmay30@gmail.com>
2023-05-17 08:17:00 +0530

    Add solution for problem `B - Email from Polycarp` Link: https://codeforces.com/contest/1185/problem/B Tags: implementation, strings, *1200 Ref: https://codeforces.com/contest/1185/submission/206166423

---
+++ codeforces/1185/B.py
@@ -0,0 +1,112 @@
+# /**
+# * author:Hisoka-TheMagician
+# * created: 17/05/2023 07:59 Chennai, India
+# **/
+        
+
+'''
+
+By titia, contest: Codeforces Round 568 (Div. 2), problem: (B) Email from Polycarp, Accepted, #, Copy
+
+'''
+
+
+#bisect.bisect_left(a, x, lo=0, hi=len(a)) is the analog of std::lower_bound()
+#bisect.bisect_right(a, x, lo=0, hi=len(a)) is the analog of std::upper_bound()
+#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
+#from collections import deque as dq #deque  e.g. myqueue=dq(list)
+#append/appendleft/appendright/pop/popleft
+#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
+#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
+#import statistics as stat  # stat.median(a), mode, mean
+#from itertools import permutations(p,r)#combinations(p,r)
+#combinations(p,r) gives r-length tuples #combinations_with_replacement
+#every element can be repeated
+        
+#Note direct assignment to check somethings doesnt work always
+#say there exists s (list) then ss=s and if we edit ss, it edits s as well
+#always try to use ss=s.copy() if u wish to make changes to ss and not reflect them in s.
+#For example: see **1379A - Acacius and String** for reference
+    
+import sys, threading, os, io 
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+from bisect import bisect_left as lower_bound
+from bisect import bisect_right as upper_bound
+from io import BytesIO, IOBase								
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+input = lambda: sys.stdin.readline().rstrip()
+def lmii():
+    return list(map(int,input().split()))
+
+def ii():
+    return int(input())
+
+def si():
+    return str(input())
+def lmsi():
+    return list(map(str,input().split()))
+def mii():
+    return map(int,input().split())
+
+def msi():
+    return map(str,input().split())
+
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
+#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+    
+    
+def solve():
+    s=si()
+    t=si()
+
+    A=[]
+    A.append([s[0],1])
+
+    for i in range(1, len(s)):
+        if s[i]==s[i-1]:
+            A[-1][1]+=1
+        else:
+            A.append([s[i],1])
+    B=[]
+    B.append([t[0],1])
+
+    for i in range(1, len(t)):
+        if t[i]==t[i-1]:
+            B[-1][1]+=1
+        else:
+            B.append([t[i],1])
+    
+    if len(A)!=len(B):
+        print("NO"); return
+    
+    for i in range(len(A)):
+        if A[i][0]!=B[i][0] or A[i][1]>B[i][1] :
+            print("NO"); return
+    
+    print("YES")
+    
+xxx=ii()
+for _ in range(xxx):
+    solve()
