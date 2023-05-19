4a1ae356c86e5e27eed229b7745ea437255fbad9
nitish <nitishkumarmay30@gmail.com>
2023-05-19 08:13:00 +0530

    Add solution for problem `B - Kuriyama Mirai's Stones` Link: https://codeforces.com/contest/433/problem/B Tags: dp, implementation, sortings, *1200 Ref: https://codeforces.com/contest/433/submission/206397605

---
+++ codeforces/433/B.py
@@ -0,0 +1,116 @@
+# /**
+# * author:Hisoka-TheMagician
+# * created: 19/05/2023 08:07 Chennai, India
+# **/
+        
+
+
+
+
+#bisect.bisect_left(a, x, lo=0, hi=len(a)) is the analog of std::lower_bound()
+#bisect.bisect_right(a, x, lo=0, hi=len(a)) is the analog of std::upper_bound()
+#from heapq import heappop,heappush,heapify #heappop(hq), heapify(list)
+#from collections import deque as dq #deque  e.g. myqueue=dq(list)
+#append/appendleft/appendright/pop/popleft
+#from bisect import bisect as bis #a=[1,3,4,6,7,8] #bis(a,5)-->3
+#import bisect #bisect.bisect_left(a,4)-->2 #bisect.bisect(a,4)-->3
+#import statistics as stat  # stat.median(a), mode, mean
+#from itertools import permutations(p,r)#combinations(p,r)
+#combinations(p,r) gives r-length tuples #combinations_with_replacement
+#every element can be repeated
+        
+#Note direct assignment to check somethings doesnt work always
+#say there exists s (list) then ss=s and if we edit ss, it edits s as well
+#always try to use ss=s.copy() if u wish to make changes to ss and not reflect them in s.
+#For example: see **1379A - Acacius and String** for reference
+    
+import sys, threading, os, io 
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+from bisect import bisect_left as lower_bound
+from bisect import bisect_right as upper_bound
+from io import BytesIO, IOBase								
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+input = lambda: sys.stdin.readline().rstrip()
+def lmii():
+    return list(map(int,input().split()))
+
+def ii():
+    return int(input())
+
+def si():
+    return str(input())
+def lmsi():
+    return list(map(str,input().split()))
+def mii():
+    return map(int,input().split())
+
+def msi():
+    return map(str,input().split())
+
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
+#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+    
+    
+    
+def solve():
+    n=ii()
+    a=lmii()
+    b=sorted(a)
+
+    prefixa =[ 0]*(n)
+    prefixa[0] =a[0]
+    for i in range(1,n):
+        prefixa[i]=prefixa[i-1]+a[i]
+    
+    
+    prefixb =[ 0]*(n)
+    prefixb[0] =b[0]
+    for i in range(1,n):
+        prefixb[i]=prefixb[i-1]+b[i]
+    
+    # print(prefixa)
+    # print(prefixb)
+
+    
+    m=ii()
+    for _ in range(m):
+        t,l,r = mii()
+        l-=1
+        r-=1
+        if t==1:
+            if l==0:
+                print(prefixa[r])
+            else:
+                print(prefixa[r]-prefixa[l-1])
+        else:
+            if l==0:
+                print(prefixb[r])
+            else:
+                print(prefixb[r]-prefixb[l-1])
+            
+    
+xxx=1
+for _ in range(xxx):
+    solve()
