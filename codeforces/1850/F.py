a3e5af179773df8cad20ac8ae9406ba02213d18d
nitish <nitishkumarmay30@gmail.com>
2023-07-22 00:09:00 +0530

    Add solution for problem `F - We Were Both Children` Link: https://codeforces.com/contest/1850/problem/F Tags: brute force, implementation, math, number theory, *1300 Ref: https://codeforces.com/contest/1850/submission/214950039

---
+++ codeforces/1850/F.py
@@ -0,0 +1,388 @@
+# /**
+# * author:Hisoka-TheMagician
+# * created: 21/07/2023 20:48 Chennai, India
+# **/
+        
+
+
+
+
+                                                                                                                        
+
+from __future__ import division, print_function
+
+import os,sys
+from io import BytesIO, IOBase
+
+if sys.version_info[0] < 3:
+    from __builtin__ import xrange as range
+    from future_builtins import ascii, filter, hex, map, oct, zip
+
+from bisect import bisect_left as lower_bound, bisect_right as upper_bound 
+
+def lmii():
+    return list(map(int,input().split()))
+
+def ii():
+    return int(input())
+
+def si():
+    return str(input())
+def lmsi():
+    return list(map(str,input().split()))
+def mii():
+    return map(int,input().split())
+
+def msi():
+    return map(str,input().split())
+
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+
+import threading
+def dmain():
+    sys.setrecursionlimit(1000000)
+    threading.stack_size(1024000)
+    thread = threading.Thread(target=main)
+    thread.start()
+
+
+def joro(L):
+    return(''.join(map(str, L)))
+
+
+def decimalToBinary(n): return bin(n).replace("0b","")
+
+
+def isprime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+
+from math import *
+def npr(n, r):
+    return factorial(n) // factorial(n - r) if n >= r else 0
+ 
+ 
+def ncr(n, r):
+    return factorial(n) // (factorial(r) * factorial(n - r)) if n >= r else 0
+ 
+ 
+def SieveOfEratosthenes(n):
+    prime = [True for i in range(n + 1)]
+    p = 2
+    li = []
+    while (p * p <= n):
+        if (prime[p] == True):
+            for i in range(p * p, n + 1, p):
+                prime[i] = False
+        p += 1
+ 
+    for p in range(2, len(prime)):
+        if prime[p]:
+            li.append(p)
+    return li
+ 
+ 
+def primefactors(n):
+    factors = []
+    while (n % 2 == 0):
+        factors.append(2)
+        n //= 2
+    for i in range(3, int(sqrt(n)) + 1, 2):  # only odd factors left
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+    if n > 2:  # incase of prime
+        factors.append(n)
+    return factors
+         
+    
+def read():
+    sys.stdin  = open('input.txt', 'r')  
+    sys.stdout = open('output.txt', 'w') 
+def tr(n):
+    return n*(n+1)//2
+
+
+from collections import Counter
+
+
+def gcd(x, y):
+    """greatest common divisor of x and y"""
+    while y:
+        x, y = y, x % y
+    return x
+
+
+def memodict(f):
+    """memoization decorator for a function taking a single argument"""
+    class memodict(dict):
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret
+
+    return memodict().__getitem__
+
+
+def pollard_rho(n):
+    """returns a random factor of n"""
+    if n & 1 == 0:
+        return 2
+    if n % 3 == 0:
+        return 3
+
+    s = ((n - 1) & (1 - n)).bit_length() - 1
+    d = n >> s
+    for a in [2, 325, 9375, 28178, 450775, 9780504, 1795265022]:
+        p = pow(a, d, n)
+        if p == 1 or p == n - 1 or a % n == 0:
+            continue
+        for _ in range(s):
+            prev = p
+            p = (p * p) % n
+            if p == 1:
+                return gcd(prev - 1, n)
+            if p == n - 1:
+                break
+        else:
+            for i in range(2, n):
+                x, y = i, (i * i + 1) % n
+                f = gcd(abs(x - y), n)
+                while f == 1:
+                    x, y = (x * x + 1) % n, (y * y + 1) % n
+                    y = (y * y + 1) % n
+                    f = gcd(abs(x - y), n)
+                if f != n:
+                    return f
+    return n
+
+
+@memodict
+def prime_factors(n):
+    """returns a Counter of the prime factorization of n"""
+    if n <= 1:
+        return Counter()
+    f = pollard_rho(n)
+    return Counter([n]) if f == n else prime_factors(f) + prime_factors(n // f)
+
+
+def distinct_factors(n):
+    """returns a list of all distinct factors of n"""
+    factors = [1]
+    for p, exp in prime_factors(n).items():
+        factors += [p**i * factor for factor in factors for i in range(1, exp + 1)]
+    return factors
+
+
+def all_factors(n):
+    """returns a sorted list of all distinct factors of n"""
+    small, large = [], []
+    for i in range(1, int(n**0.5) + 1, 2 if n & 1 else 1):
+        if not n % i:
+            small.append(i)
+            large.append(n // i)
+    if small[-1] == large[-1]:
+        large.pop()
+    large.reverse()
+    small.extend(large)
+    return small
+        
+from collections import Counter, defaultdict, deque
+import math
+def solve():
+    import sys
+    input =sys.stdin.buffer.readline
+    
+    n =ii()
+    a=lmii()
+
+    isone=0
+    if 1 in a:
+        isone=a.count(1)
+
+
+    dic=defaultdict(int)
+    
+    for x in a:
+        if 2<=x<=n:
+            dic[x]+=1
+    # print(dic)
+    seen = set()
+
+    maxi =0
+    for i in range(n,1,-1):
+        if i in seen:
+            continue
+        facs = all_factors(i)
+
+        # print(i,facs)
+        cnt =0
+        for x in facs:
+            cnt+=dic[x]
+            seen.add(x)
+        maxi = max(cnt,maxi)
+    
+    print(maxi+isone)
+        
+
+
+
+
+    
+
+
+    
+
+    
+    
+        
+            
+            
+def main():
+    for i in range(ii()):
+        solve()
+                
+            
+                
+                
+                
+                
+            
+    
+            
+            
+    
+    
+    
+    
+        
+            
+    
+            
+            
+            
+        
+    
+        
+    
+    
+        
+        
+        
+
+            
+                    
+                
+                    
+                    
+                    
+                    
+                    
+                    
+        
+                
+        
+        
+       
+    
+           
+          
+          
+                
+            
+        
+                
+    
+        
+        
+        
+       
+            
+                
+        
+
+
+
+
+# region fastio
+# template taken from https://github.com/cheran-senthil/PyRival/blob/master/templates/template.py
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+def print(*args, **kwargs):
+    """Prints the values to a stream, or to sys.stdout by default."""
+    sep, file = kwargs.pop("sep", " "), kwargs.pop("file", sys.stdout)
+    at_start = True
+    for x in args:
+        if not at_start:
+            file.write(sep)
+        file.write(str(x))
+        at_start = False
+    file.write(kwargs.pop("end", "\n"))
+    if kwargs.pop("flush", False):
+        file.flush()
+
+
+if sys.version_info[0] < 3:
+    sys.stdin, sys.stdout = FastIO(sys.stdin), FastIO(sys.stdout)
+else:
+    sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+# endregion
+
+
+if __name__ == "__main__":
+    # read()
+    main()
+    #dmain()
+
+# Comment Read()
