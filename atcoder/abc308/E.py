6fbc67c9b48102e96f311cc7533a4af4e33c8ea4
nitish <nitishkumarmay30@gmail.com>
2023-08-04 13:24:00 +0530

    Add solution for problem `E - MEX` Link: https://atcoder.jp/contests/abc308/tasks/abc308_e Tags: AtCoder, *475 Ref: https://atcoder.jp/contests/abc308/submissions/44227157

--- atcoder/abc308/E.py
+++ atcoder/abc308/E.py
@@ -1,28 +1,310 @@
-import sys
-input = sys.stdin.readline
-n=int(input())
-a=list(map(int,input().split()))
-s=input().rstrip()
-cntx=[0]*3
-for i in range(n):
-  if s[i]=="X":
-    cntx[a[i]]+=1
-cntm=[0]*3
-ans=0
-for i in range(n):
-  if s[i]=="M":
-    cntm[a[i]]+=1
-  elif s[i]=="X":
-    cntx[a[i]]-=1
-  else:
-    for x in range(3):
-      for y in range(3):
-        se=set()
-        se.add(a[i])
-        se.add(x)
-        se.add(y)
-        now=0
-        while now in se:
-          now+=1
-        ans+=now*cntm[x]*cntx[y]
-print(ans)
+# /**
+# * author:Hisoka-TheMagician
+# * created: 04/08/2023 11:29 Chennai, India
+# **/
+        
+
+
+
+
+                                                                                                                        
+
+from __future__ import division, print_function
+
+import os,sys
+#sys.setrecursionlimit(9*10**8)
+from io import BytesIO, IOBase
+
+if sys.version_info[0] < 3:
+    from __builtin__ import xrange as range
+    from future_builtins import ascii, filter, hex, map, oct, zip
+
+from bisect import bisect_left as lower_bound, bisect_right as upper_bound 
+
+from math import sqrt
+def lmii():
+    return list(map(int,input().split()))
+
+def ii():
+    return int(input())
+
+def si():
+    return str(input())
+def lmsi():
+    return list(map(str,input().split()))
+def mii():
+    return map(int,input().split())
+
+def msi():
+    return map(str,input().split())
+
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+
+import threading
+def dmain():
+    sys.setrecursionlimit(1000000)
+    threading.stack_size(1024000)
+    thread = threading.Thread(target=main)
+    thread.start()
+
+
+def joro(L):
+    return(''.join(map(str, L)))
+
+
+def decimalToBinary(n): return bin(n).replace("0b","")
+
+
+def isprime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+
+ 
+ 
+def SieveOfEratosthenes(n):
+    prime = [True for i in range(n + 1)]
+    p = 2
+    li = []
+    while (p * p <= n):
+        if (prime[p] == True):
+            for i in range(p * p, n + 1, p):
+                prime[i] = False
+        p += 1
+ 
+    for p in range(2, len(prime)):
+        if prime[p]:
+            li.append(p)
+    return li
+ 
+ 
+def primefactors(n):
+    factors = []
+    while (n % 2 == 0):
+        factors.append(2)
+        n //= 2
+    for i in range(3, int(sqrt(n)) + 1, 2):  # only odd factors left
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+    if n > 2:  # incase of prime
+        factors.append(n)
+    return factors
+         
+    
+from os import path
+def read():
+     if (path.exists('input.txt')):
+        sys.stdin  = open('input.txt', 'r')  
+        sys.stdout = open('output.txt', 'w') 
+ 
+def tr(n):
+    return n*(n+1)//2
+
+
+        
+from collections import Counter, defaultdict, deque
+
+def solve():
+    import sys
+    input =sys.stdin.buffer.readline
+    
+    n=ii()
+    a=lmii()
+    s=list(si())
+
+    left =[[0]*4 for _ in range(n+1)]
+    right = [[0]*4 for _ in range(n+1)]
+
+    ans =0 
+
+    def mex(a,b,c):
+        v =[0]*3
+
+        v[a]=1
+        v[b]=1
+        v[c]=1
+
+        for i in range(3):
+            if not v[i]:
+                return i
+        
+        return 3
+    for i in range(n):
+        left[i+1]=left[i].copy()
+        if s[i]=="M":
+            left[i+1][a[i]]+=1
+    
+
+
+    for i in range(n-1,-1,-1):
+        right[i] = right[i+1].copy()
+        if s[i]=="X":
+            right[i][a[i]]+=1
+    
+
+    
+    for i in range(n):
+        if s[i]!="E":
+            continue
+            
+        for j in range(3):
+            for k in range(3):
+                ans += left[i][j] * right[i][k] * mex(j,a[i],k)
+
+    print(ans)
+    
+
+    
+    
+        
+            
+            
+def main():
+    # for i in range(ii()):
+        solve()
+                
+            
+                
+                
+                
+                
+            
+    
+            
+            
+    
+    
+    
+    
+        
+            
+    
+            
+            
+            
+        
+    
+        
+    
+    
+        
+        
+        
+
+            
+                    
+                
+                    
+                    
+                    
+                    
+                    
+                    
+        
+                
+        
+        
+       
+    
+           
+          
+          
+                
+            
+        
+                
+    
+        
+        
+        
+       
+            
+                
+        
+
+
+
+
+# region fastio
+# template taken from https://github.com/cheran-senthil/PyRival/blob/master/templates/template.py
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+def print(*args, **kwargs):
+    """Prints the values to a stream, or to sys.stdout by default."""
+    sep, file = kwargs.pop("sep", " "), kwargs.pop("file", sys.stdout)
+    at_start = True
+    for x in args:
+        if not at_start:
+            file.write(sep)
+        file.write(str(x))
+        at_start = False
+    file.write(kwargs.pop("end", "\n"))
+    if kwargs.pop("flush", False):
+        file.flush()
+
+
+if sys.version_info[0] < 3:
+    sys.stdin, sys.stdout = FastIO(sys.stdin), FastIO(sys.stdout)
+else:
+    sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+# endregion
+
+
+if __name__ == "__main__":
+    read()
+    main()
+    #dmain()
+
+# Comment Read()
