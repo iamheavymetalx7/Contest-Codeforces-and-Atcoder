4ca22f96d735c3576f78bc23e9d4b38e08a94c77
nitish <nitishkumarmay30@gmail.com>
2024-01-12 09:05:00 +0530

    Add solution for problem `A - 202<s>3</s>` Link: https://atcoder.jp/contests/abc335/tasks/abc335_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc335/submissions/49227239

---
+++ atcoder/abc335/A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+    using namespace std;
+    #define int long long
+    #define pi (3.141592653589)
+    #define mod 1000000007
+    #define float double
+    #define ff first
+    #define ss second
+    #define mk make_pair
+    #define pb push_back
+    #define rep(i, start, end) for (int i = start; i < end; i++)
+    #define ld long double
+    #define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int inf = 1000000000000000000;
+    using ii = pair<int, int>;
+    
+    void solve()
+    {
+        string s;
+        cin>>s;
+        s[s.size()-1]='4';
+        cout<<s<<endl;
+    
+    
+    }
+    
+    signed main()
+    {
+        fast
+        // int t; cin>>t; while(t--)
+    
+        #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+        #endif
+        solve();
+        return 0;
+    }
